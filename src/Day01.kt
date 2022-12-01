877d5db924b46db220468d4a4b43533b541afb19
dominik003 <info@dominik-lammers.de>
2022-12-01 22:42:24 +0100

    feat: Add day 01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
+import java.util.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxVal: Int = Int.MIN_VALUE
+        var cur = 0
+
+        input.forEach {
+            if (it == "") {
+                maxVal = if (cur > maxVal) cur else maxVal
+                cur = 0
+            } else {
+                cur += Integer.parseInt(it)
+            }
+        }
+        return maxVal
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = PriorityQueue<Int>()
+        var cur = 0
+
+        input.forEach {
+            if (it == "") {
+                calories.add(cur)
+                cur = 0
+
+                if (calories.size > 3) {
+                    calories.poll()
+                }
+            } else {
+                cur += Integer.parseInt(it)
+            }
+        }
+        return calories.sumOf { it }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    (testInput as MutableList<String>).add("")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
+    (input as MutableList<String>).add("")
     println(part1(input))
     println(part2(input))
 }
